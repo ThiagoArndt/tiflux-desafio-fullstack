e2c00d61735c3c16bb175848ca5899c6d1773ea5
Thiago Arndt <thiagoarndt04@gmail.com>
2024-04-02 21:19:15 -0300

    BACKEND - prisma setup

---
+++ backend/src/prisma/index.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisaClient = new PrismaClient();
+
+export default prisaClient;
---
+++ backend/src/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+
+export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+
+ //TODO: add routes
+    
+}
